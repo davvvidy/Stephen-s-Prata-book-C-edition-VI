3881712a213b3bee349bea2a437288875a02f682
davvvidy <przemekdawidowski97@gmail.com>
2020-03-24 08:11:46 +0100

    First exercise vol2

---
+++ Name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "What's ur name?" << endl;
+    cin  >> name;
+    cout << "Author's name is " << name << endl;
+    return 0;
+}
+
