cbd2563533ff61b88436e4e1844e7cd3cd24a7e0
davvvidy <przemekdawidowski97@gmail.com>
2020-03-25 09:30:15 +0100

    Chapter two exercise two

---
+++ Converter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Number of  miles: " << endl;
+    int miles;
+    cin >> miles;
+    cout << miles << " miles is " << miles*1852 << " meters." << endl;
+    //system("pause");
+    return 0;
+}
